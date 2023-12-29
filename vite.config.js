2c92b9cf1090cfaeb515146932ea175ef558fec7
veraliu <veraaaaaliuuuuu@gmail.com>
2023-12-29 16:04:48 +0800

    feat: add base config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/NeutecLimited-testing/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "/src"),
