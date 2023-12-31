b029b953e34b3bb333bacfb6e8309b7136b1a922
veraliu <veraaaaaliuuuuu@gmail.com>
2023-12-31 10:57:51 +0800

    feat: add Button component

---
+++ src/components/Button/Index.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+    color: String,
+    background: String,
+})
+</script>
+
+<template>
+    <button type="button">
+        <slot name="wording"></slot>
+    </button>
+</template>
+
+<style lang="scss" scoped>
+    button{
+        background-color: #fff;
+        border: 1px solid #000;
+        padding: 0 5px;
+        font-size: 13px;
+        cursor: pointer;
+    }
+</style>
